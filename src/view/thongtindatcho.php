55b383568426aab8d574348c8d1d2402ab6cc551
LeHoaiDuyBinh <101654026+LeHoaiDuyBinh@users.noreply.github.com>
2023-04-26 23:08:38 +0700

    fix menu

--- src/view/thongtindatcho.php
+++ src/view/thongtindatcho.php
@@ -59,6 +59,13 @@
                 </div>
             </div>
 
+<!-- Animation Header -->
+<script>
+const link = document.querySelector('.nav-link.thongtin');
+  link.style.borderBottom = '3px solid transparent';
+  link.style.transition = 'border-color 0.3s ease-in-out';
+  link.style.borderColor = '#01b3a7';
+</script>            
 
 
 
