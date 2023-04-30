222c4829f7f095c29b9d22d75e7bfe788460d100
LeHoaiDuyBinh <101654026+LeHoaiDuyBinh@users.noreply.github.com>
2023-04-30 14:29:08 +0700

    fix form đăng nhập

--- src/view/admin/login.php
+++ src/view/admin/login.php
@@ -10,8 +10,8 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
     <!-- Stylesheets-->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,500,600,700%7CPoppins:400%7CTeko:300,400">
     <link rel="stylesheet" href="/view/css/admin.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
@@ -37,7 +37,7 @@ session_start();
                   <p><?php echo $_SESSION['message']; ?></p>
                   <?php unset($_SESSION['message']); ?>
                 <?php endif; ?>
-                <!-- <div class="alert alert-danger">Username or password is incorrect.</div> -->
+                <div style="width: 43vh; left: -1vh;" class="alert alert-danger">Username or password is incorrect.</div>
                 <div class="form-group">
                     <i class="far fa-user"></i> 
                     <input type="email" class="form-input" placeholder="Email" name="Email">
