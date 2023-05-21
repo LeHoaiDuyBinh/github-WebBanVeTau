42fabba9c0a569a54161fc8a6d361a586a6e49e8
HaiTrieu17 <101725971+HaiTrieu17@users.noreply.github.com>
2023-05-21 14:13:51 +0700

    Update DatVeController.php
    add dienthongtin()

--- src/controller/DatVeController.php
+++ src/controller/DatVeController.php
@@ -3,5 +3,8 @@
         public function index(){
             include 'view/ticketing/BookTickets/ketquatimve.php';
         }
+        public function dienthongtin(){
+            include 'view/ticketing/BookTickets/dienthongtin.php';
+        }
     }
 ?>
